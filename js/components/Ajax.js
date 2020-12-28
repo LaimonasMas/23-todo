715d008012ab6c8f82bf7f903b46a8df210df853
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2020-12-28 17:10:23 +0200

    Ajax

--- js/components/Ajax.js
+++ js/components/Ajax.js
@@ -2,6 +2,6 @@ class Ajax {
     constructor() {
         this.serverURL = '';
         this.requestType = 'GET';       // get, post, put, delete, options...
-        this.targetInfo = '';
+        this.targetInfo = 'todos.json';
     }
 }
